696c57993390eda2b0fa850eb161dd29671e8a29
rodricksnyc <rodricksnyc@gmail.com>
2017-10-08 19:58:17 -0400

    my commit

--- seedTweets.js
+++ seedTweets.js
@@ -5,7 +5,8 @@ function seedTweets () {
   Tweets.find({}, (err, res) => {
     console.log(res)
     if (res.length === 0) {
-      var tweets = ['/img/tweet0.png', '/img/tweet1.png', '/img/tweet2.png', '/img/tweet3.png', '/img/tweet4.png', '/img/tweet04.png', '/img/tweet5.png', '/img/tweet6.png', '/img/tweet7.png', '/img/tweet8.png', '/img/tweet9.png', '/img/tweet10.png']
+
+      var tweets = ['/img/tweet0.png', '/img/tweet00.png', '/img/tweet1.png', '/img/tweet2.png', '/img/tweet3.png', '/img/tweet4.png', '/img/tweet04.png', '/img/tweet5.png', '/img/tweet6.png', '/img/tweet7.png', '/img/tweet8.png', '/img/tweet9.png', '/img/tweet10.png']
 
       tweets.map(function(tweet){
         var tweetToAdd = {
